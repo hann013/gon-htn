c6fa020623e54f7de7b70d5278340f178cefffd4
Dayeong Lee <leedayeong6@gmail.com>
2015-09-19 12:44:31 -0400

    database objects

---
+++ app/src/main/java/firebase_objects/Event.java
@@ -0,0 +1,15 @@
+package firebase_objects;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by dayeonglee on 15-09-19.
+ */
+public class Event
+{
+    private String[] items;
+    private Timestamp date;
+    private String location;
+
+
+}
